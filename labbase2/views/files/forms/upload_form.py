974d740ca18d9eccdca0bd41b65eaa669ef1de08
Raphael Schleutker <raphael.schleutker@uni-muenster.de>
2024-07-22 20:49:12 +0200

    Restructure table toolbar.

--- labbase2/views/files/forms/upload_form.py
+++ labbase2/views/files/forms/upload_form.py
@@ -24,14 +24,12 @@ class UploadFile(EditForm):
     file = FileField(
         "Select File",
         validators=[DataRequired()],
-        render_kw=RENDER_KW_FILE | {"id": "edit-form-upload-filepath",
-                                    "type": "file"}
+        render_kw=RENDER_KW_FILE
     )
     note = TextAreaField(
         "Note",
         validators=[Optional(), Length(max=256)],
         filters=[strip_input],
-        render_kw=RENDER_KW | {"id": "edit-form-upload-note",
-                               "placeholder": "Note",
+        render_kw=RENDER_KW | {"placeholder": "Note",
                                "rows": 8}
     )
